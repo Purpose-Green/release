4af72e99fea5e69a187aea418457850212cbba5c
Chemaclass <jose.valera@purpose-green.com>
2024-09-17 20:07:13 +0200

    demo change, so we can create a new release

--- src/io.sh
+++ src/io.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
+# shellcheck disable=SC2155
 function io::confirm_or_exit() {
-  # shellcheck disable=SC2155
   local txt=$(echo -e "$1 (Y/n):")
   read -p "$txt " -n 1 -r
   echo
