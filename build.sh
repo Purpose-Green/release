f9c3ac9a963207c419f4f38710de6f3acc29308c
Chemaclass <jose.valera@purpose-green.com>
2024-09-18 08:58:43 +0200

    feat: add --new option on build.sh

--- build.sh
+++ build.sh
@@ -49,10 +49,58 @@ function generate_checksum() {
   echo "$checksum"
 }
 
+function update_version() {
+  local version_type=$1
+  local version_file=$2
+
+  local current_version
+  current_version=$(cat "$version_file")
+  IFS='.' read -r major minor patch <<< "$current_version"
+
+  case "$version_type" in
+    major)
+      major=$((major + 1))
+      ;;
+    minor)
+      minor=$((minor + 1))
+      ;;
+    patch)
+      patch=$((patch + 1))
+      ;;
+    *)
+      echo "Invalid version increment option: $version_type. Use major, minor, or patch."
+      exit 1
+      ;;
+  esac
+
+  local new_version="$major.$minor.$patch"
+  echo "$new_version" > "$version_file"
+  echo "Version updated to $new_version"
+}
+
 ########################
 #         MAIN         #
 ########################
-OUT_DIR=${1:-"bin"}
+OUT_DIR="bin"
+NEW_VERSION_TYPE=""
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --new)
+      shift
+      NEW_VERSION_TYPE="$1"
+      ;;
+    *)
+      OUT_DIR="$1"
+      ;;
+  esac
+  shift
+done
+
+if [[ "$NEW_VERSION_TYPE" != "" ]]; then
+  update_version "$NEW_VERSION_TYPE" "src/version.txt"
+fi
 
 mkdir -p "$OUT_DIR"
 build "$OUT_DIR/deploy"
