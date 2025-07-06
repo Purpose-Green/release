4f02e0bed7be946aa2fdb930778866343e9de77b
Chemaclass <jose.valera@purpose-green.com>
2025-07-06 17:36:26 +0200

    set bashunit:0.21.0 - mocks are fixed

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -10,7 +10,7 @@
 
 # Add latest bashunit release to your dependencies
 DEPENDENCIES=(
-  "https://github.com/TypedDevs/bashunit/releases/download/0.18.0/bashunit"
+  "https://github.com/TypedDevs/bashunit/releases/download/0.21.0/bashunit"
   "https://github.com/Chemaclass/create-pr/releases/download/0.8.0/create-pr"
   "https://github.com/Chemaclass/bash-dumper/releases/download/0.1/dumper.sh@dev"
 )
